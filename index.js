214a1ed010634fd21b281fa33e12df5cc524e29a
Kat Tochowicz <ktochow1@binghamton.edu>
2020-05-18 08:00:44 -0400

    postFetch() - works

--- index.js
+++ index.js
@@ -26,5 +26,22 @@ function getPosts(){
 
 function createFormHandler(e){
   e.preventDefault();
-  console.log(e)
+  const formTitle = document.querySelector("#input-title").value;
+  const formUrl = document.querySelector("#input-url").value;
+  postFetch(formTitle, formUrl);
+  // const submit = document.querySelector("#submit-button");
+}
+
+function postFetch(title, url){
+  let bodyData = {title, url}
+  fetch(endpoint, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(bodyData)//how i'm going to SEND data to API
+  })
+  .then(response => response.json())
+  .then(post => {
+    console.log(post);
+  })
+
 }
