29f34dee8171444c8c7f958dd9819ca5787980d8
Kat Tochowicz <ktochow1@binghamton.edu>
2020-06-01 00:42:58 -0400

    postFetch() notes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 const endpoint = "http://localhost:3000/api/v1/posts"
 
-
-
 document.addEventListener("DOMContentLoaded", () => {
   getPosts();
-
-  // editBtn.addEventListener("click", function(){
-  //   alert("click");
-  // })
   const createPostForm = document.querySelector('#post-form');
   createPostForm.addEventListener("submit", (e) => {
     createFormHandler(e)
@@ -19,14 +13,11 @@ function getPosts(){
   .then(response => response.json())
   .then(posts => {
     posts.forEach(post => {
-      // const markup =
-      // `<div data-id=${post.id}>
-      //   <h3>${post.title}</h3><br>
-      //   <img src=${post.image_url} height="200" width="250"><br>
-      //   </div>`;
-      let postContainer = document.querySelector("#post-container");
-      let divContainer = document.createElement("div");
-      divContainer.setAttribute("data-id", `${post.id}`);
+
+      let divContainer = document.querySelector("#div-container");
+      let postContainer = document.createElement("div");
+      postContainer.setAttribute("id", "post-container");
+      postContainer.setAttribute("data-id", `${post.id}`);
 
       let h3 = document.createElement("h3");
       let h3Text = document.createTextNode(`${post.title}`)
@@ -38,67 +29,19 @@ function getPosts(){
       let editBtn = document.createElement("button");
       let btnTxt = document.createTextNode("Edit");
 
-
-
-      postContainer.appendChild(divContainer)
-      divContainer.appendChild(h3)
+      divContainer.appendChild(postContainer)
+      postContainer.appendChild(h3)
       h3.appendChild(h3Text)
-      divContainer.appendChild(img)
-      divContainer.appendChild(editBtn)
+      postContainer.appendChild(img)
+      postContainer.appendChild(editBtn)
       editBtn.appendChild(btnTxt)
 
       editBtn.addEventListener("click", function(){
         console.log("click")
       })
-
-
-      // let node = document.createTextNode(`${post.title}`)
-      // div.append(node)
-
-
-      // let postContainer = document.querySelector("#post-container")
-      // let mainDiv = document.createElement("div")
-      // postContainer.appendChild(mainDiv)
-        // mainDiv.setAttribute("data-id", `${post.id}`)
-      // let editBtn = document.createElement('button');
-      // editBtn.value = "Edit"
-      // editBtn.setAttribute(`data-id`, `data[${posts.id}].id`);
-        // editBtn.setAttribute(`id`, `edit-button`);
-      // editBtn.id = post.id
-      // mainDiv.appendChild(editBtn)
-        // const editBtn = document.getElementById("#edit-button");
-        // const container = document.querySelector('#post-container').innerHTML += markup;
-        //
-        // let img = document.querySelector('img');
-        // img.append(editBtn)
-        // editBtn.append(postDiv)
-        // editBtn.append(postDiv)
-
-          //<button id="edit-button" data-id=${post.id}>Edit</button>
-
-
-        // console.log(editBtn)
-        // let array = markup.split(", ")
-
-        // console.log(editBtn)
-
-
-        // editBtn.addEventListener("click", function(){
-        //   alert("click")
-        })
-        })
-        // editBtn.addEventListener("click", function(){
-        //   alert("click");
-        // console.log(editBtn)
-
-
-        // const editBtn = document.getElementById("#edit-button").addEventListener("click", () => { console.log("click") });
-        //
-        // editBtn.addEventListener('click', click)
-        // editBtn.addEventListener('click', () => {console.log("click")})
-
-
-  };
+    })
+  })
+};
 
 
 function createFormHandler(e){
@@ -124,14 +67,18 @@ function postFetch(title, image_url){
   })
   .then(response => response.json())
   .then(post => {
-    const postData = post.data.attributes
-    const postMarkup = `
-    <div data-id=${post.id}>
-    <h3>${postData.title}</h3>
-    <img src=${postData.image_url} height="200" width="250">
-    <button data-id=${postData.id}>Edit</button>
-    </div>`;
-    document.querySelector("#post-container").innerHTML += postMarkup;
+    console.log(post.title)
+    // CREATE POST ELEMENTS AFTER POST-REQ SUBMISSION
+
+
+    // const postData = post.data.attributes
+    // const postMarkup = `
+    // <div data-id=${post.id}>
+    // <h3>${postData.title}</h3>
+    // <img src=${postData.image_url} height="200" width="250">
+    // <button data-id=${postData.id}>Edit</button>
+    // </div>`;
+    // document.querySelector("#post-container").innerHTML += postMarkup;
   })
 }
 
