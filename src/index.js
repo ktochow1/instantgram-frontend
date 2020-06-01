2b8759c2cfeb87af7607ffa020a92d5f2e3c1197
Kat Tochowicz <ktochow1@binghamton.edu>
2020-06-01 00:14:31 -0400

    DOM manipulation

--- src/index.js
+++ src/index.js
@@ -19,18 +19,53 @@ function getPosts(){
   .then(response => response.json())
   .then(posts => {
     posts.forEach(post => {
-      const markup = `<div data-id=${post.id}>
-        <h3>${post.title}</h3><br>
-        <img src=${post.image_url} height="200" width="250"><br>
-        <button id="edit-button" data-id=${post.id}>Edit</button><br>
-        </div>`;
+      // const markup =
+      // `<div data-id=${post.id}>
+      //   <h3>${post.title}</h3><br>
+      //   <img src=${post.image_url} height="200" width="250"><br>
+      //   </div>`;
+      let postContainer = document.querySelector("#post-container");
+      postContainer.setAttribute("data-id", `${post.id}`);
 
-        document.querySelector('#post-container').innerHTML += markup;
-        const editBtn = document.getElementById("#edit-button");
-        console.log(editBtn)
+      let h3 = document.createElement("h3");
+      let h3Text = document.createTextNode(`${post.title}`)
+      h3.appendChild(h3Text)
+      // h3.value = post.title;
+      postContainer.appendChild(h3)
+      let node = document.createTextNode(`${post.title}`)
+      // div.append(node)
+
+
+      // let postContainer = document.querySelector("#post-container")
+      // let mainDiv = document.createElement("div")
+      // postContainer.appendChild(mainDiv)
+        // mainDiv.setAttribute("data-id", `${post.id}`)
+      // let editBtn = document.createElement('button');
+      // editBtn.value = "Edit"
+      // editBtn.setAttribute(`data-id`, `data[${posts.id}].id`);
+        // editBtn.setAttribute(`id`, `edit-button`);
+      // editBtn.id = post.id
+      // mainDiv.appendChild(editBtn)
+        // const editBtn = document.getElementById("#edit-button");
+        // const container = document.querySelector('#post-container').innerHTML += markup;
+        //
+        // let img = document.querySelector('img');
+        // img.append(editBtn)
+        // editBtn.append(postDiv)
+        // editBtn.append(postDiv)
+
+          //<button id="edit-button" data-id=${post.id}>Edit</button>
+
+
+        // console.log(editBtn)
+        // let array = markup.split(", ")
+
+        // console.log(editBtn)
+
+
         // editBtn.addEventListener("click", function(){
         //   alert("click")
-        // })
+        })
         })
         // editBtn.addEventListener("click", function(){
         //   alert("click");
@@ -41,7 +76,7 @@ function getPosts(){
         //
         // editBtn.addEventListener('click', click)
         // editBtn.addEventListener('click', () => {console.log("click")})
-    });
+
 
   };
 
